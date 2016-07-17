c9f7b263ff4cb67242b204590e1304b3d5980e3b
Martijn de Ruiter <martijn@MacBook-Pro-van-Martijn.local>
2016-07-17 23:13:20 +0200

    update

--- assets/src/js/build.js
+++ assets/src/js/build.js
@@ -1,1 +1,5 @@
-alert('hello');
+import $ from 'jQuery';
+import TweenMax from 'gsap';
+$(document).ready(function(){
+  console.log('Your app has started! It has loaded jQuery & TweenMax');
+});
--- index.php
+++ index.php
@@ -2,11 +2,11 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Default</title>
+    <title>New Project</title>
     <link rel="stylesheet" href="/assets/css/style.css" media="screen" title="no title" charset="utf-8">
   </head>
   <body>
-    <h1>Test app</h1>
+    <h1>Your app is running!!</h1>
   </body>
   <script src="/assets/js/scripts.js" charset="utf-8"></script>
 </html>
